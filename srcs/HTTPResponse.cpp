56b7e70fd2f12e7eb85effdc38221bf13b006cee
juhanse <julien.hanse12@gmail.com>
2026-02-20 12:28:26 +0100

    refactor: HTTPResponse class

--- srcs/HTTPResponse.cpp
+++ srcs/HTTPResponse.cpp
@@ -6,48 +6,48 @@ HTTPResponse::~HTTPResponse() {}
 std::map<int, std::string> HTTPResponse::_status_messages = HTTPResponse::_initStatusMessages();
 
 std::map<int, std::string> HTTPResponse::_initStatusMessages() {
-    std::map<int, std::string> status;
+	std::map<int, std::string> status;
 
-    status[200] = "OK";
-    status[201] = "Created";
-    status[204] = "No Content";
-    status[301] = "Moved Permanently";
-    status[400] = "Bad Request";
-    status[403] = "Forbidden";
-    status[404] = "Not Found";
-    status[405] = "Method Not Allowed";
-    status[413] = "Payload Too Large";
-    status[500] = "Internal Server Error";
-    status[502] = "Bad Gateway";
+	status[200] = "OK";
+	status[201] = "Created";
+	status[204] = "No Content";
+	status[301] = "Moved Permanently";
+	status[400] = "Bad Request";
+	status[403] = "Forbidden";
+	status[404] = "Not Found";
+	status[405] = "Method Not Allowed";
+	status[413] = "Payload Too Large";
+	status[500] = "Internal Server Error";
+	status[502] = "Bad Gateway";
 
-    return status;
-}
-
-std::string HTTPResponse::getRawResponse() const {
-    std::stringstream res;
-    // On récupère le message correspondant au code
-    std::string msg = _status_messages.count(_status_code) ? _status_messages.at(_status_code) : "Unknown Error";
-
-    res << "HTTP/1.0 " << _status_code << " " << msg << "\r\n";
-    // ... suite des headers et body
-    return res.str();
+	return status;
 }
 
 void HTTPResponse::setStatusCode(int code) {
-    _status_code = code;
+	_status_code = code;
 }
 
 void HTTPResponse::setHeader(const std::string& key, const std::string& value) {
-    _headers[key] = value;
+	_headers[key] = value;
 }
 
 void HTTPResponse::setBody(const std::string& content) {
-    _body = content;
+	_body = content;
 
-    // Conversion de la taille en string (C++98 style)
-    std::stringstream ss;
-    ss << _body.size();
-    
-    // On met à jour le header indispensable pour le navigateur
-    setHeader("Content-Length", ss.str());
+	// Conversion de la taille en string (C++98 style)
+	std::stringstream ss;
+	ss << _body.size();
+
+	// On met à jour le header indispensable pour le navigateur
+	setHeader("Content-Length", ss.str());
+}
+
+std::string HTTPResponse::getRawResponse() const {
+	std::stringstream res;
+	// On récupère le message correspondant au code
+	std::string msg = _status_messages.count(_status_code) ? _status_messages.at(_status_code) : "Unknown Error";
+
+	res << "HTTP/1.0 " << _status_code << " " << msg << "\r\n";
+	// ... suite des headers et body
+	return res.str();
 }
--- srcs/HTTPResponse.hpp
+++ srcs/HTTPResponse.hpp
@@ -8,26 +8,25 @@
 #include <sstream>
 
 class HTTPResponse {
-    private:
-        int                                _status_code;
-        static std::map<int, std::string> _status_messages;
-        std::string                        _body;
-        std::map<std::string, std::string> _headers;
+	private:
+		int									_status_code;
+		std::string							_body;
+		std::map<std::string, std::string>	_headers;
+		static std::map<int, std::string>	_status_messages;
+		static std::map<int, std::string>	_initStatusMessages();
 
-        static std::map<int, std::string> _initStatusMessages();
+	public:
+		HTTPResponse();
+		~HTTPResponse();
 
-    public:
-        HTTPResponse();
-        ~HTTPResponse();
+		void setStatusCode(int code);
+		void setHeader(const std::string& key, const std::string& value);
+		void setBody(const std::string& content);
 
-        void setStatusCode(int code);
-        void setHeader(const std::string& key, const std::string& value);
-        void setBody(const std::string& content);
-        
-        // Helpers
-        void setContentType(const std::string& path); // Déduit le MIME type (.html, .jpg...)
-        void generateErrorPage(int code, const ServerConfig& config);
+		// Helpers
+		void setContentType(const std::string& path); // MIME type
+		void generateErrorPage(int code, const ServerConfig& config);
 
-        // La méthode main pour Antonia
-        std::string getRawResponse() const; 
+		// La méthode main -> Antonia
+		std::string getRawResponse() const; 
 };
