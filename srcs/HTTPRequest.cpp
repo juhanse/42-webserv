fddbeb18231ba6f2b97fe6277c3a021f8a8abd11
juhanse <julien.hanse12@gmail.com>
2026-02-28 14:46:42 +0100

    refactor: HTTPRequest class

--- srcs/HTTPRequest.cpp
+++ srcs/HTTPRequest.cpp
@@ -4,37 +4,34 @@ HTTPRequest::HTTPRequest() {}
 HTTPRequest::~HTTPRequest() {}
 
 void HTTPRequest::parse(const std::string& rawData) {
-    std::istringstream stream(rawData);
-    std::string line;
+	std::istringstream stream(rawData);
+	std::string line;
 
-    // 1. Parsing de la première ligne (ex: GET /index.html?id=1 HTTP/1.1)
-    if (std::getline(stream, line)) {
-        std::istringstream lineStream(line);
-        lineStream >> _method >> _path >> _protocol;
+	if (std::getline(stream, line)) {
+		std::istringstream lineStream(line);
+		lineStream >> _method >> _path >> _protocol;
 
-        // Extraction de la query string (après le ?)
-        size_t questionMarkPos = _path.find('?');
-        if (questionMarkPos != std::string::npos) {
-            _query = _path.substr(questionMarkPos + 1);
-            _path = _path.substr(0, questionMarkPos);
-        }
-    }
+		size_t questionMarkPos = _path.find('?');
+		if (questionMarkPos != std::string::npos) {
+			_query = _path.substr(questionMarkPos + 1);
+			_path = _path.substr(0, questionMarkPos);
+		}
+	}
 
-    // 2. Parsing des Headers
-    while (std::getline(stream, line) && line != "\r" && !line.empty()) {
-        size_t colonPos = line.find(':');
-        if (colonPos != std::string::npos) {
-            std::string key = line.substr(0, colonPos);
-            std::string value = line.substr(colonPos + 2); // +2 pour sauter ": "
-            // Nettoyage du \r à la fin si présent
-            if (!value.empty() && value[value.size() - 1] == '\r')
-                value.erase(value.size() - 1);
-            _headers[key] = value;
-        }
-    }
+	while (std::getline(stream, line) && line != "\r" && !line.empty()) {
+		size_t colonPos = line.find(':');
+		if (colonPos != std::string::npos) {
+			std::string key = line.substr(0, colonPos);
+			std::string value = line.substr(colonPos + 2);
 
-    // 3. Le reste est le corps (Body)
-    std::stringstream bodyStream;
-    bodyStream << stream.rdbuf();
-    _body = bodyStream.str();
+			if (!value.empty() && value[value.size() - 1] == '\r') {
+				value.erase(value.size() - 1);
+			}
+			_headers[key] = value;
+		}
+	}
+
+	std::stringstream bodyStream;
+	bodyStream << stream.rdbuf();
+	_body = bodyStream.str();
 }
