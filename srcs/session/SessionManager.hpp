f27fa4514b5220429505486a45571342c8d95700
juhanse <julien.hanse12@gmail.com>
2026-03-10 12:31:11 +0100

    add: SessionManager class

---
+++ srcs/session/SessionManager.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <map>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+
+struct SessionData {
+	std::string username;
+	time_t expires_at;
+};
+
+class SessionManager {
+	private:
+		std::map<std::string, SessionData> _sessions;
+		std::string _generateSessionID();
+
+	public:
+		SessionManager();
+		~SessionManager();
+
+		std::string createSession(const std::string& username, int ttl_seconds);
+		bool isValidSession(const std::string& session_id);
+		SessionData getSession(const std::string& session_id);
+		void cleanup();
+};
