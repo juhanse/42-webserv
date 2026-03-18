8f778e371e511f39adfce02d65fc1faaf8963bdc
juhanse <julien.hanse12@gmail.com>
2026-03-18 13:27:51 +0100

    delete: crash cgi

--- srcs/www/cgi-bin/bonus.sh
+++ srcs/www/cgi-bin/bonus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -ne "Content-Type: text/html\r\n\r\n"
-echo "<html><body><h1>CGI Bash</h1><p>Bonus valide ! Je suis un script Bash, pas Python.</p></body></html>"
+echo "<html><body><h1>CGI Bash</h1><p>Je suis un script Bash, pas Python.</p></body></html>"
